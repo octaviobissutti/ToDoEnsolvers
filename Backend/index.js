43cd19d8d67ebccc6ad54026033f8b728d322a31
kf380 <kevinfavre@hotmail.com>
2021-08-06 14:42:21 -0300

    actualizacion

--- Backend/index.js
+++ Backend/index.js
@@ -1,24 +1,15 @@
-const express = require('express');
+const server = require('./app');
+const { conn } = require('./db.js');
 
-const routers = require('./routers');
-const path = require('path');
-require('dotenv').config({path: '.env'})
+// Syncing all the models at once.
+conn.sync({ force: true }).then(() => {
+  server.listen(3001, () => {
+    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  });
+});
 
 
-const db = require('./db');
 
-require('./models/Folder');
-require('./models/Todo');
 
-db.sync()
-    .then(() => console.log('Conectado al Servidor'))
-    .catch(error => console.log(error));
 
-const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use('/', routers());
-
-app.listen(3000);
--- Backend/models/Todo.js
+++ Backend/models/Todo.js
@@ -1,20 +1,22 @@
-const {DataTypes} = require('sequelize');
-const db = require('../db');
+
+const { DataTypes } = require("sequelize");
 
 
-const Todo = sequelize.define('todo', {
-    id: {
+module.exports = (sequelize) => {
+  
+  sequelize.define(
+    "todo",
+    {
+      id: {
         type: DataTypes.UUID,
         defaultValue: DataTypes.UUIDV4,
         primaryKey: true,
-    }, 
-    name:{
-        type: DataTypes.STRING,
-    },
-    state:{
+      },
+      name: { type: DataTypes.STRING, allowNull: false },
+      state:{
         type: DataTypes.BOOLEAN,
     }
-});
-
-
-module.exports = Todo;
+    },
+    { timestamps: false }
+  );
+};
